d5e95fe184ef4fcb132981860079a625d901f2ef
Jackson Luckey <jacksonmluckey@gmail.com>
2024-03-02 18:33:00 +0100

    Can now upload and display both the projects and student preferences

--- src/app.py
+++ src/app.py
@@ -4,10 +4,13 @@ from werkzeug.utils import secure_filename
 import pandas as pd
 
 UPLOAD_FOLDER = '../src/uploads'
-ALLOWED_EXTENSIONS = {'txt', 'csv', 'xlsx'}
+# TODO add support for CSV
+ALLOWED_EXTENSIONS = {'txt', 'xlsx'}
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'NOTASECRETONLYUSEFORDEBUGGING'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -27,26 +30,40 @@ def txt_to_df(txt_file):
 
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def home():
     if request.method == 'POST':
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
+        if 'student_preferences' not in request.files or 'projects' not in request.files:
+            flash('Need both student_preferences and projects')
             return redirect(request.url)
-        file = request.files['file']
+        student_preferences = request.files['student_preferences']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
-        if file.filename == '':
+        if student_preferences.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            if file_type(file.filename) == 'txt':
-                file_contents = txt_to_df(file)
-                return render_template('home.html', student_preferences = file_contents)
-            if file_type(file.filename) == 'xlsx':
-                preferences_df = pd.read_excel(file)
-                return render_template('home.html', student_preferences = preferences_df)
+        if student_preferences and allowed_file(student_preferences.filename):
+            student_preferences_filename = secure_filename(student_preferences.filename)
+            student_preferences.save(os.path.join(app.config['UPLOAD_FOLDER'], student_preferences_filename))
+            if file_type(student_preferences.filename) == 'txt':
+                student_preferencess_df = txt_to_df(student_preferences)
+            if file_type(student_preferences.filename) == 'xlsx':
+                student_preferencess_df = pd.read_excel(student_preferences)
 
-    return render_template('home.html', student_preferences = '')
+        projects = request.files['projects']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if projects.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if projects and allowed_file(projects.filename):
+            projects_filename = secure_filename(projects.filename)
+            projects.save(os.path.join(app.config['UPLOAD_FOLDER'], projects_filename))
+            if file_type(projects.filename) == 'txt':
+                projects_df = txt_to_df(projects)
+            if file_type(projects.filename) == 'xlsx':
+                projects_df = pd.read_excel(projects)
+            
+        return render_template('home.html', student_preferences = student_preferencess_df, projects = projects_df)
+
+    return render_template('home.html', student_preferences = '', projects = projects_df)
