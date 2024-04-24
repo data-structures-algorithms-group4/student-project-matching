e38b33c6ab69dffc7a4dd762bc6452c0f3d32b43
lonnychen <lonny.cl.chen@gmail.com>
2024-04-24 12:46:44 +0200

    Roll-up all "test-dev" changes into one commit on this branch
    matching_algorithm.py
    - replace "print" with "logging"
    stable_match_checker.py
    - replace "print" with "logging"
    - add check for preferred project availability
    - add check for unmatched students
    testing_environment.py
    - add logging configuration
    test_algorithm.py
    - add logging and two new test functions

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -4,23 +4,38 @@
 #   Usage in terminal/PyCharm: pytest test_algorithm.py
 
 import pytest
-from tests.testing_environment import run_and_check_test_data
+from tests.testing_environment import run_and_check_test_data, config_logging
 
 ##############
 # Unit Tests #
 ##############
+# def config_logging(log_filename: str, log_level_str: str)
+# log_level_str options: 'DEBUG', 'INFO' (default), 'WARNING', 'ERROR', 'CRITICAL'
 
+#@pytest.mark.skip()
 def test_td_1(students_filename = 'td_1_students.xlsx', projects_filename = 'td_1_projects.xlsx'):
-    ''' Run and check test case: test data set 1.
-        Expected result: PASS '''
+    ''' Test data set 1. Expect: PASS'''
+    config_logging('test_td_1.log', 'DEBUG')
     result, message = run_and_check_test_data(students_filename, projects_filename)
     assert result, message
-    print(message)  # otherwise, "PASS" message doesn't get printed
 
 #@pytest.mark.skip()
 def test_td_2(students_filename = 'td_2_students.xlsx', projects_filename = 'td_2_projects.xlsx'):
-    ''' Run and check test case: test data set 2.
-        Expected result: PASS '''
+    ''' Test data set 2. Expect: PASS'''
+    config_logging('test_td_2.log', 'DEBUG')
     result, message = run_and_check_test_data(students_filename, projects_filename)
     assert result, message
-    print(message)  # otherwise, "PASS" message doesn't get printed
+
+@pytest.mark.skip()
+def test_p_not_chosen(students_filename = 's_project_not_chosen.xlsx', projects_filename = 'p_project_not_chosen.xlsx'):
+    ''' One project not chosen by any students. Expect: PASS'''
+    config_logging('test_p_not_chosen.log')
+    result, message = run_and_check_test_data(students_filename, projects_filename)
+    assert result, message
+
+@pytest.mark.skip()
+def test_s_vary_p_pref_size(students_filename = 's_varying_preferences_to_projects.xlsx', projects_filename = 'p_varying_preferences_to_projects.xlsx'):
+    ''' Vary size of S preference list of Ps. Expect: PASS'''
+    config_logging('test_s_vary_p_pref_size.log')
+    result, message = run_and_check_test_data(students_filename, projects_filename)
+    assert result, message
